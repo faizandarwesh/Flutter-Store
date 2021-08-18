019065233a229c15f4f5452727195009f556d3d1
Faizan Darwesh <faizandarwesh@outlook.com>
2021-08-18 10:27:08 -0700

    Adding shimmer loading effect in Products UI

--- lib/custom_widgets/ProductListTile.dart
+++ lib/custom_widgets/ProductListTile.dart
@@ -2,45 +2,97 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/products/model/Product.dart';
 import 'package:flutter_ui/products/ui/ProductDetailScreen.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ProductListTile extends StatelessWidget {
   final Product product;
+  final bool isLoading;
 
-  ProductListTile({required this.product});
+  ProductListTile({required this.product, required this.isLoading});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: ()=> Navigator.push(context, CupertinoPageRoute(builder: (context) => ProductDetailScreen(product: product,))),
+      onTap: () => Navigator.push(
+          context,
+          CupertinoPageRoute(
+              builder: (context) => ProductDetailScreen(
+                    product: product,
+                  ))),
       child: Hero(
         tag: product.imageLink,
-        child: Card(
-          elevation: 8,
-          margin: const EdgeInsets.all(8.0),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Image.network(product.imageLink,fit: BoxFit.cover,),
-                ListTile(
-                  title: Text(
-                    product.name,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  subtitle: Text(
-                    '\$${product.price}',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700,color: Colors.black),
+        child: isLoading
+            ? Card(
+                elevation: 8,
+                margin: const EdgeInsets.all(8.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                child: Column(
+                  children: [
+                    Shimmer.fromColors(
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                      child: Container(
+                        width: 200,
+                        height: 180,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    ListTile(
+                      title: Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.grey[100]!,
+                        child: Container(
+                          height: 16,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                      subtitle: Shimmer.fromColors(
+                        baseColor: Colors.grey[300]!,
+                        highlightColor: Colors.grey[100]!,
+                        child: Container(
+                          height: 16,
+                          color: Colors.grey[300],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            : Card(
+                elevation: 8,
+                margin: const EdgeInsets.all(8.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Image.network(
+                        product.imageLink,
+                        fit: BoxFit.cover,
+                      ),
+                      ListTile(
+                        title: Text(
+                          product.name,
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        subtitle: Text(
+                          '\$${product.price}',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
+              ),
       ),
     );
   }
--- lib/products/ui/ProductsScreen.dart
+++ lib/products/ui/ProductsScreen.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui/custom_widgets/CustomAppBar.dart';
 import 'package:flutter_ui/custom_widgets/ProductListTile.dart';
 import 'package:flutter_ui/products/controller/ProductsController.dart';
+import 'package:flutter_ui/products/model/Product.dart';
 import 'package:get/get.dart';
 
 class ProductScreen extends StatelessWidget {
   final controller = Get.put(ProductsController());
+  final product = Product(id: 1,description: "Description",imageLink: "",name: "Maybelline",price: "32");
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +17,20 @@ class ProductScreen extends StatelessWidget {
         ),
         body: Obx(
           () => controller.isLoading.value
-              ? Center(child: CircularProgressIndicator())
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, mainAxisExtent: 300),
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 6,
+                  itemBuilder: (_, index) {
+                    return ProductListTile(product:product,isLoading: controller.isLoading.value);
+                  })
               : GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,mainAxisExtent: 300),
+                      crossAxisCount: 2, mainAxisExtent: 300),
                   itemCount: controller.products.length,
                   itemBuilder: (_, index) {
-                    return ProductListTile(product: controller.products[index]);
+                    return ProductListTile(product: controller.products[index],isLoading: controller.isLoading.value,);
                   }),
         ));
   }
