860c0e7aabcae29dabc971e309358e13a869ef1d
Faizan Darwesh <faizandarwesh@outlook.com>
2021-08-18 11:50:42 -0700

    Adding design items to Product Detail Screen

--- lib/products/ui/ProductDetailScreen.dart
+++ lib/products/ui/ProductDetailScreen.dart
@@ -4,32 +4,175 @@ import 'package:google_fonts/google_fonts.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   final Product product;
-  const ProductDetailScreen({Key? key,required this.product}) : super(key: key);
+
+  const ProductDetailScreen({Key? key, required this.product})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    var counter = 1;
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Hero(
                 tag: product.imageLink,
                 child: Container(
                   width: double.infinity,
                   height: 400,
-                  child: Image.network(product.imageLink,fit: BoxFit.cover,),),
+                  child: Image.network(
+                    product.imageLink,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               ),
-              SizedBox(height: 16,),
+              SizedBox(
+                height: 16,
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(product.name,style: GoogleFonts.lato(fontSize: 18,fontWeight: FontWeight.w700),),
+                child: Text(
+                  product.name,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.lato(
+                      fontSize: 22, fontWeight: FontWeight.w700),
+                ),
               ),
-              SizedBox(height: 16,),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      '\$${product.price}',
+                      style: GoogleFonts.nunito(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.black),
+                    ),
+                  ),
+                  Container(
+                      height: 60,
+                      margin: EdgeInsets.only(left: 16),
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border(
+                            top: BorderSide(width: 2, color: Colors.blue[300]!),
+                            bottom:
+                            BorderSide(width: 2, color: Colors.blue[300]!),
+                            left:
+                            BorderSide(width: 2, color: Colors.blue[300]!),
+                            right:
+                            BorderSide(width: 2, color: Colors.blue[300]!)),
+                      ),
+                      child: IconButton(
+                        onPressed: null,
+                        icon: Icon(Icons.remove, color: Colors.blue),
+                      )),
+                  Text(
+                    "$counter",
+                    style: GoogleFonts.nunitoSans(
+                        fontSize: 22, fontWeight: FontWeight.w700),
+                  ),
+                  Container(
+                      margin: EdgeInsets.only(right: 16),
+                      height: 60,
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border(
+                            top: BorderSide(width: 2, color: Colors.blue[300]!),
+                            bottom:
+                            BorderSide(width: 2, color: Colors.blue[300]!),
+                            left:
+                            BorderSide(width: 2, color: Colors.blue[300]!),
+                            right:
+                            BorderSide(width: 2, color: Colors.blue[300]!)),
+                      ),
+                      child: IconButton(
+                        onPressed: (){
+
+                        },
+                        icon: Icon(Icons.add, color: Colors.blue),
+                      ))
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(product.description,style: GoogleFonts.nunito(fontSize: 16,fontWeight: FontWeight.w700),),
+                child: Text(
+                  "Description",
+                  style: GoogleFonts.lato(
+                      fontSize: 22,fontWeight: FontWeight.w700),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  product.description,
+                  style: GoogleFonts.nunito(
+                      fontSize: 18),
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                      height: 60,
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        border: Border(
+                            top: BorderSide(width: 2, color: Colors.blue[300]!),
+                            bottom:
+                                BorderSide(width: 2, color: Colors.blue[300]!),
+                            left:
+                                BorderSide(width: 2, color: Colors.blue[300]!),
+                            right:
+                                BorderSide(width: 2, color: Colors.blue[300]!)),
+                      ),
+                      child: IconButton(
+                        onPressed: null,
+                        icon: Icon(Icons.add_shopping_cart, color: Colors.blue),
+                      )),
+                  Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 64, vertical: 8),
+                        child: TextButton(
+                            onPressed: null,
+                            child: Text(
+                              "BUY NOW",
+                              style: GoogleFonts.lato(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            )),
+                      )),
+                ],
+              ),
+              SizedBox(
+                height: 16,
               ),
             ],
           ),
--- lib/products/ui/ProductsScreen.dart
+++ lib/products/ui/ProductsScreen.dart
@@ -6,8 +6,14 @@ import 'package:flutter_ui/products/model/Product.dart';
 import 'package:get/get.dart';
 
 class ProductScreen extends StatelessWidget {
-  final controller = Get.put(ProductsController());
-  final product = Product(id: 1,description: "Description",imageLink: "",name: "Maybelline",price: "32");
+  final controller = Get.put(ProductsController());  //initialization of GetX ProductController
+  final product = Product(
+      id: 1,
+      description: "Description",
+      imageLink: "https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png",
+      name: "Maybelline",
+      price: "32");
+  //default product object for shimmer effect null object issue
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +22,26 @@ class ProductScreen extends StatelessWidget {
           height: 60,
         ),
         body: Obx(
-          () => controller.isLoading.value
+          () => controller.isLoading.value  //if isLoading is True then show Shimmer effect else show Product List
               ? GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2, mainAxisExtent: 300),
                   physics: NeverScrollableScrollPhysics(),
                   itemCount: 6,
                   itemBuilder: (_, index) {
-                    return ProductListTile(product:product,isLoading: controller.isLoading.value);
+                    return ProductListTile(
+                        product: product,
+                        isLoading: controller.isLoading.value);
                   })
               : GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2, mainAxisExtent: 300),
                   itemCount: controller.products.length,
                   itemBuilder: (_, index) {
-                    return ProductListTile(product: controller.products[index],isLoading: controller.isLoading.value,);
+                    return ProductListTile(
+                      product: controller.products[index],
+                      isLoading: controller.isLoading.value,
+                    );
                   }),
         ));
   }
