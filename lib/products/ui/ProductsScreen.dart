d9ec37e9a4e6e1696c2c8b81ce9cd46ddc306922
Faizan Darwesh <faizandarwesh@outlook.com>
2021-08-19 01:32:26 -0700

    Add scaffold key for opening drawer

--- lib/products/ui/ProductsScreen.dart
+++ lib/products/ui/ProductsScreen.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui/custom_widgets/CustomAppBar.dart';
+import 'package:flutter_ui/custom_widgets/CustomDrawer.dart';
 import 'package:flutter_ui/custom_widgets/ProductListTile.dart';
 import 'package:flutter_ui/products/controller/ProductsController.dart';
 import 'package:flutter_ui/products/model/Product.dart';
 import 'package:get/get.dart';
 
 class ProductScreen extends StatelessWidget {
-  final controller = Get.put(ProductsController());  //initialization of GetX ProductController
+  final controller =
+      Get.put(ProductsController()); //initialization of GetX ProductController
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   final product = Product(
       id: 1,
       description: "Description",
-      imageLink: "https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png",
+      imageLink:
+          "https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png",
       name: "Maybelline",
       price: "32");
+
   //default product object for shimmer effect null object issue
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        key: _scaffoldKey,
         appBar: CustomAppBar(
           height: 60,
+          globalKey: _scaffoldKey,
         ),
+        drawer: CustomDrawer(),
         body: Obx(
-          () => controller.isLoading.value  //if isLoading is True then show Shimmer effect else show Product List
+          () => controller.isLoading
+                  .value //if isLoading is True then show Shimmer effect else show Product List
               ? GridView.builder(
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2, mainAxisExtent: 300),
